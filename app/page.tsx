cce5de5f652c2fe4f3ec7a38c270a7b57031c157
Flooooooooooorian <florian.weber@neuefische.de>
2025-12-09 13:01:59 +0100

    ws-lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,31 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import useWebSocket from "react-use-websocket";
 
 interface OutgoingMessage {
   event: string;
-  data: unknown;
+  data: string;
 }
 
 export default function HomePage() {
   const [messageInput, setMessageInput] = useState("");
 
-  const [socket, setSocket] = useState<WebSocket | null>(null);
   const [messages, setMessages] = useState<unknown[]>([]);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(
+    "ws://localhost:3000/"
+  );
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:3000/ws");
-
-    ws.onopen = () => {
-      console.log("Connected to Backend Successfully.");
-    };
-
-    ws.onmessage = (event) => {
-      console.log("Message from server:", event);
-      try {
-        const data = JSON.parse(event.data.toString());
-        setMessages((prev) => [...prev, data]);
-      } catch (e) {
-        console.error("Error parsing message:", e, event.data);
-      }
-    };
-
-    ws.onclose = () => {
-      console.log("WebSocket disconnected.");
-      setSocket(null);
-    };
-
-    ws.onerror = (error) => {
-      console.error("WebSocket error:", error);
-    };
-
-    setSocket(ws);
-  }, []);
-
-  const sendMessage = () => {
-    if (socket) {
-      const outgoingMessage: OutgoingMessage = {
-        event: "message",
-        data: messageInput,
-      };
-
-      socket.send(JSON.stringify(outgoingMessage));
+    if (lastMessage !== null) {
+      setMessages((prev) => [...prev, JSON.parse(lastMessage.data)]);
     }
-  };
+  }, [lastMessage]);
 
   return (
     <div>
       <h1>Next.js WebSocket Client</h1>
-      <p>Status: {socket ? "Connected" : "Disconnected"}</p>
+      <p>Status: {readyState === 1 ? "Connected" : "Disconnected"}</p>
 
       <input
         type="text"
@@ -64,7 +33,15 @@ export default function HomePage() {
         onChange={(e) => setMessageInput(e.target.value)}
         placeholder="Type your message..."
       />
-      <button onClick={sendMessage}>Send</button>
+      <button
+        onClick={() =>
+          sendMessage(
+            JSON.stringify({ event: "currywurst", data: messageInput })
+          )
+        }
+      >
+        Send
+      </button>
       <h2>Latest Server Message:</h2>
       <div>
         {messages.map((msg, index) => (
